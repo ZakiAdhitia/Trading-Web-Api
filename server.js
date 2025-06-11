05db6c84ebcc7da618892e41a4b6ecd5c251db45
ZakiAdhitia <zakiadhitia@smkwikrama.sch.id>
2025-06-11 20:41:17 +0700

    sasxa

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 require('dotenv').config();
-
+const fs = require('fs');
+const https = require('https');
 const express = require('express');
 const cors = require('cors');
 
@@ -7,7 +8,13 @@ const configRoutes = require('./routes/configRoutes');
 const orderRoutes = require('./routes/orderRoutes');
 
 const app = express();
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 8443; // gunakan port di atas 1024
+
+// Baca SSL certificate dan private key
+const sslOptions = {
+  key: fs.readFileSync('./ssl/key.pem'),
+  cert: fs.readFileSync('./ssl/cert.pem'),
+};
 
 app.use(cors());
 app.use(express.json());
@@ -17,9 +24,10 @@ app.use('/orders', orderRoutes);
 app.use('/webhook', orderRoutes);
 
 app.get('/', (req, res) => {
-  res.send('API berjalan!');
+  res.send('API berjalan dengan HTTPS!');
 });
 
-app.listen(PORT, '0.0.0.0', () => {
-  console.log(`Server berjalan di http://0.0.0.0:${PORT}`);
+// Buat HTTPS server
+https.createServer(sslOptions, app).listen(PORT, '0.0.0.0', () => {
+  console.log(`Server HTTPS berjalan di https://0.0.0.0:${PORT}`);
 });
