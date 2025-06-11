12a87591db93be4a3c702646b01d8cfb188b8862
ZakiAdhitia <zakiadhitia@smkwikrama.sch.id>
2025-06-11 20:13:14 +0700

    axsa

--- server.js
+++ server.js
@@ -1,7 +1,5 @@
 require('dotenv').config();
 
-const fs = require('fs');
-const https = require('https');
 const express = require('express');
 const cors = require('cors');
 
@@ -9,7 +7,7 @@ const configRoutes = require('./routes/configRoutes');
 const orderRoutes = require('./routes/orderRoutes');
 
 const app = express();
-const PORT = process.env.PORT || 443;
+const PORT = process.env.PORT || 5000;
 
 app.use(cors());
 app.use(express.json());
@@ -19,14 +17,9 @@ app.use('/orders', orderRoutes);
 app.use('/webhook', orderRoutes);
 
 app.get('/', (req, res) => {
-  res.send('API berjalan dengan HTTPS!');
+  res.send('API berjalan!');
 });
 
-const sslOptions = {
-  key: fs.readFileSync('./ssl/key.pem'),
-  cert: fs.readFileSync('./ssl/cert.pem')
-};
-
-https.createServer(sslOptions, app).listen(PORT, '0.0.0.0', () => {
-  console.log(`✅ Server HTTPS berjalan di https://0.0.0.0:${PORT}`);
+app.listen(PORT, '0.0.0.0', () => {
+  console.log(`Server berjalan di http://0.0.0.0:${PORT}`);
 });
